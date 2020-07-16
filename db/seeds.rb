a5ffc7b3e98ee85ffb424ee9a11b02ea69f245fa
Danny Rosenblatt <dgrosenblatt@gmail.com>
2020-07-15 21:47:39 -0400

    Add more books to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,8 +16,8 @@ end
 
 alice = Book.find_or_create_by(name: 'Alice In Wonderland') do |book|
   book.author = 'Lewis Carroll'
-  book.cover_image_thumb_s3_key = 'alice-in-wonderland/alice-in-wonderland-thumb.jpg'
   book.cover_image_full_s3_key = 'alice-in-wonderland/alice-in-wonderland-full.jpg'
+  book.cover_image_thumb_s3_key = 'alice-in-wonderland/alice-in-wonderland-thumb.jpg'
 end
 12.times do |n|
   Chapter.find_or_create_by(position: n+1, book_id: alice.id) do |chapter|
@@ -27,4 +27,49 @@ end
 end
 if alice.persisted?
   puts 'Seeded Alice In Wonderland'
+end
+
+sherlock = Book.find_or_create_by(name: 'The Adventures of Sherlock Holmes') do |book|
+  book.author = 'Arthur Conan Doyle'
+  book.cover_image_full_s3_key = 'sherlock-holmes/sherlock-holmes-full.jpg'
+  book.cover_image_thumb_s3_key = 'sherlock-holmes/sherlock-holmes-thumb.jpg'
+end
+12.times do |n|
+  Chapter.find_or_create_by(position: n+1, book_id: sherlock.id) do |chapter|
+    chapter.name = "Chapter #{n+1}"
+    chapter.text_s3_key = "sherlock-holmes/chapters/chapter#{n+1}.html"
+  end
+end
+if sherlock.persisted?
+  puts 'Seeded Sherlock Holmes'
+end
+
+uncle_toms_cabin = Book.find_or_create_by(name: "Uncle Tom's Cabin") do |book|
+  book.author = 'Harriet Beecher Stowe'
+  book.cover_image_full_s3_key = 'uncle-toms-cabin/uncle-toms-cabin-full.jpg'
+  book.cover_image_thumb_s3_key = 'uncle-toms-cabin/uncle-toms-cabin-thumb.jpg'
+end
+45.times do |n|
+  Chapter.find_or_create_by(position: n+1, book_id: uncle_toms_cabin.id) do |chapter|
+    chapter.name = "Chapter #{n+1}"
+    chapter.text_s3_key = "uncle-toms-cabin/chapters/chapter#{n+1}.html"
+  end
+end
+if uncle_toms_cabin.persisted?
+  puts "Seeded Uncle Tom's Cabin"
+end
+
+call_of_the_wild = Book.find_or_create_by(name: 'The Call of the Wild') do |book|
+  book.author = 'Jack London'
+  book.cover_image_full_s3_key = 'the-call-of-the-wild/call-of-the-wild-full.jpg'
+  book.cover_image_thumb_s3_key = 'the-call-of-the-wild/call-of-the-wild-thumb.jpg'
+end
+7.times do |n|
+  Chapter.find_or_create_by(position: n+1, book_id: call_of_the_wild.id) do |chapter|
+    chapter.name = "Chapter #{n+1}"
+    chapter.text_s3_key = "the-call-of-the-wild/chapters/chapter#{n+1}.html"
+  end
+end
+if call_of_the_wild.persisted?
+  puts 'Seeded The Call of the Wild'
 end
