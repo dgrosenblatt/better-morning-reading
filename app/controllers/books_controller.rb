48ab809a11336d15d99fda721e9762db55e8816c
Danny Rosenblatt <dgrosenblatt@gmail.com>
2020-07-16 16:44:31 -0400

    Add book search

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,22 @@
 class BooksController < ApplicationController
   # Guest users can view collection of Books
   def index
-    # TODO: add pagination
-    @books = Book.all
+    @query = params[:query]
+    search_term = params[:query]&.downcase
+
+    if search_term.present?
+      @books = Book.where('LOWER(name) ilike ?', "%#{search_term}%")
+        .or(Book.where('LOWER(author) ilike ?', "%#{search_term}%"))
+        .order(order)
+        .limit(20)
+    else
+      @books = Book.all.order(order).limit(20)
+    end
+  end
+
+  private
+
+  def order
+    { name: :asc }
   end
 end
