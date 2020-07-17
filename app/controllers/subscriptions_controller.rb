68bad9cf66be376917a67420d2dffff98f68b4a7
Danny Rosenblatt <dgrosenblatt@gmail.com>
2020-07-17 14:10:46 -0400

    Add edit/update subscriptions

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -5,24 +5,46 @@ class SubscriptionsController < AuthenticatedController
   end
 
   def create
-    @subscription = Subscription.new(subscription_params)
+    @subscription = Subscription.new(create_subscription_params)
 
     if @subscription.save
       if params[:now] == '1'
         @subscription.quick_start!
       end
 
-      redirect_to me_path, notice: 'Subscription created'
+      redirect_to me_path, notice: 'Subscription created.'
     else
       @book = Book.find(params[:book_id])
       render :new
     end
   end
 
-  def subscription_params
+  def edit
+    @book = Book.find(params[:book_id])
+    @subscription = Subscription.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:book_id])
+    @subscription = Subscription.find(params[:id])
+
+    if @subscription.update(update_subscription_params)
+      redirect_to me_path, notice: 'Weekly schedule updated.'
+    else
+      render :edit
+    end
+  end
+
+  def create_subscription_params
     params
       .require(:subscription)
       .permit(:sunday, :monday, :tuesday, :wednesday, :thursday, :friday, :saturday)
       .merge(book_id: params[:book_id], user_id: current_user.id, status: 'active')
+  end
+
+  def update_subscription_params
+    params
+      .require(:subscription)
+      .permit(:sunday, :monday, :tuesday, :wednesday, :thursday, :friday, :saturday)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   get '/me', to: 'current_user#show'
   resources :books, only: :index do
-    resources :subscriptions, only: [:new, :create]
+    resources :subscriptions, only: [:new, :create, :edit, :update]
   end
   resources :stripe_customer_subscriptions, only: :create
 end
